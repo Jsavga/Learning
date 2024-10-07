cf80f2e36d721a6536eec9c913d106a4c1b84bce
Jsavga <digphan@yahoo.com>
2024-10-06 22:25:34 -0400

    added files from PCC chapter 8 after starting to work with modules and wrinting first module call Also created a new directory for the files and this commit/push is to see how GIT handles it.

--- working_with_modules/first_module.py
+++
@@ -1,9 +0,0 @@
-#This is the function portion of previous codings so that I can learn use modules
-#this function code with be imported as a module into another file.
-
-def make_pizza(*toppings):
-    """Print the list of toppings that have been requested"""
-
-    print('\nMaking a pizza with the following toppings:')
-    for topping in toppings:
-        print(f'- {topping}')
